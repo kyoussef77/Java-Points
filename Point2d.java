52434d2758cff4e6a323bb20b5ef419ce1192878
Kamal <ky83@njit.edu>
2018-10-01 21:54:19 -0400

    added files

---
+++ Point2d.java
@@ -0,0 +1,49 @@
+public class Point2d{
+
+	int x;
+	int y;
+
+	public Point2d(){}
+
+	public Point2d(int x, int y){
+		setX(x);
+		setY(y);
+	}
+	public void setX(int x){
+		this.x = x;
+	}
+
+
+	public int getX(){
+		return x;
+	}
+
+	public void setY(int y){
+		this.y = y;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public float distance(Point2d.other){
+		float xdiff = other.getX() - getX();
+		float ydiff = other.getY() - getY();
+		float sqrdiff = xdiff*xdiff + ydiff * ydiff;
+		float d = Math.sqrt(sqrdiff);
+		return d;
+	}
+
+	public static void main(String[] args){
+
+		Point2d p1 = new Point2d(3,5);
+		Point2d p2 = new Point2d(3,10);
+
+		System.out.println(p1.distance(p2));
+
+	}
+
+
+
+
+}
---
+++ Point3d.java
@@ -0,0 +1,43 @@
+public class Point3d extends Point2d {
+
+  private int z;
+
+  public Point3d(){}
+
+  public Point3d(int x, int y, int z) {
+    super(x,y);
+    setZ(z);
+}
+
+  public void setZ(int z) {
+    this.z = z;
+  }
+
+  public int getZ() {
+    return z;
+  }
+
+  public float getDistance(Point2d other) {
+    return super.distance(other);
+  }
+
+  public float distance(Point3d other) {
+    Point2d tempPoint = new Point2d();
+    tempPoint.setX(other.x);
+    tempPoint.setY(other.y);
+
+    float temp = getDistance(tempPoint);
+    float zdiff = other.getZ() - getZ();
+
+    float sqrdiff =  temp + zdiff*zdiff;
+    float d = (float) Math.sqrt(sqrdiff);
+    return d;
+
+  }
+  public static void main(String[] args) {
+      Point3d p1 = new Point3d(3,5,5);
+      Point3d p2 = new Point3d(3,10,5);
+
+      System.out.println(p1.distance(p2));
+    }
+}
